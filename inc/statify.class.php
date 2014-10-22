8e8eb4ebf5f535f54263cd40415905267cac3ae5
sergej.mueller <sergej.mueller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-22 12:02:12 +0000

    Add: Wishlist link
    git-svn-id: https://plugins.svn.wordpress.org/statify/trunk@1012073 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/statify.class.php
+++ inc/statify.class.php
@@ -150,8 +150,9 @@ class Statify
 		return array_merge(
 			$input,
 			array(
+				'<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=ZAQUT9RLPW8QN" target="_blank">PayPal</a>',
 				'<a href="https://flattr.com/t/1733733" target="_blank">Flattr</a>',
-				'<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=ZAQUT9RLPW8QN" target="_blank">PayPal</a>'
+				'<a href="https://www.amazon.de/registry/wishlist/2U5I7F9649LOJ/" target="_blank">Wishlist</a>'
 			)
 		);
 	}
